0037304f9d9e2619b376e393e14409636dee9894
Jun Young Cho <godguman@gmail.com>
2020-09-01 20:12:12 +0700

    usdvnd

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
-let amount = parseInt(prompt("amount?")); // a number OR NaN
-let fromcurrency = prompt("from currency");
-let tocurrency = prompt("to currency");
+// let amount = parseInt(prompt("amount?"));
+// let fromcurrency = prompt("from currency");
+// let tocurrency = prompt("to currency");
 let ratio = 23208;
 let result = 0;
 
-function usdToVnd() {
+function con() {
+	let amount = document.getElementById("inputAmount").value;
+
+	if (isNaN(amount) == true) {
+		alert("please type a number");
+		return;
+	}
+
+	let fromcurrency = document.getElementById("fromcurrencylist").value;
+	let tocurrency = document.getElementById("tocurrencylist").value;
+
+	if (fromcurrency === "USD" && tocurrency === "VND") {
+		usdToVnd(amount, fromcurrency, tocurrency);
+	} else if (fromcurrency === "VND" && tocurrency === "USD") {
+		vndToUsd(amount, fromcurrency, tocurrency);
+	}
+	document.getElementById(
+		"resultArea"
+	).innerHTML = `convert amount is ${result}`;
+}
+
+function usdToVnd(amount, fromCurrency, tocurrency) {
 	result = amount * ratio;
+	result = formatCurrency(tocurrency, result);
 }
 
-function vndToUsd() {
+function vndToUsd(amount, fromCurrency, tocurrency) {
 	result = amount / ratio;
+	result = formatCurrency(tocurrency, result);
+}
+function formatCurrency(type, value) {
+	const formatter = new Intl.NumberFormat(type, {
+		currency: type,
+		style: "currency",
+	});
+	return formatter.format(value);
 }
 
-// function currencyamount(
-// console.log(amount = document.getElementById("amount").value;);
-// )
-
-if (isNaN(amount) === false) {
-	if (
-		(fromcurrency === "usd" || fromcurrency === "Usd") &&
-		(tocurrency === "vnd" || tocurrency === "Vnd")
-	) {
-		usdToVnd();
-	}
-	if (
-		(fromcurrency === "vnd" || fromcurrency === "Vnd") &&
-		(tocurrency === "usd" || tocurrency === "Usd")
-	) {
-		vndToUsd();
-	}
-
-	console.log(amount, fromcurrency, "in", tocurrency, "is", result);
-} else alert("please enter a number");
-
-// document.getElementById("amount")
-
-// parseInt(){
-// if isNaN()
+// if (isNaN(amount) === false) {
+// if (
+// (fromcurrency === "usd" || fromcurrency === "Usd") &&
+// (tocurrency === "vnd" || tocurrency === "Vnd")
+// ) {
+// usdToVnd();
+// }
+// if (
+// (fromcurrency === "vnd" || fromcurrency === "Vnd") &&
+// (tocurrency === "usd" || tocurrency === "Usd")
+// ) {
+// vndToUsd();
 // }
 
-// parseInt("ty4yty") == NaN
-
-// if (isNaN("10") == false ) {
-
-// }
+// console.log(amount, fromcurrency, "in", tocurrency, "is", result);
+// } else alert("please enter a number");
